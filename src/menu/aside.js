2763a1c191cc8c2b076b86a9a4f2a2d24951bf55
lichao <xiaochaoli212@163.com>
2019-11-14 11:21:04 +0800

    update ui header

--- src/menu/aside.js
+++ src/menu/aside.js
@@ -3,7 +3,7 @@ import demoFrame from './modules/demo-frame'
 export default [
   { path: '/index', title: '首页', icon: 'home' },
   // {
-  //   //title: '人像卡口',
+  //   title: '人像卡口',
   //   icon: 'folder-o',
   //   children: [
   //     { path: '/page1', title: '抓拍查询' },
@@ -12,95 +12,89 @@ export default [
   //   ]
   // },
   {
-    title: '监控布控中心',
-    icon: 'television',
+    title: '大数据分析',
+    icon: 'linode',
     children: [
-      // { path: '/face-search', title: '结构化监控查询', icon: 'gg-circle' },
-      // { path: '/face-match', title: '通过人脸查询视频', icon: 'newspaper-o' },
-      { path: '/liveview', title: '实时监控显示', icon: 'play' },
-      { path: '/real-video', title: '地图布控显示', icon: 'support' },
-      { path: '/stream-list', title: '历史监控查询', icon: 'magic' }
+      { path: '/people-analyse', title: '人口数据分析', icon: 'tint' },
+      { path: '/enterprise-analyse', title: '企业数据分析', icon: 'tint' },
+      { path: '/wea-analyse', title: '水电煤数据分析', icon: 'tint' },
+      { path: '/express-analyse', title: '快递数据分析', icon: 'tint' },
+      { path: '/ss-analyse', title: '社保数据分析', icon: 'tint' }
     ]
   },
   {
-    title: '轨迹追踪',
-    icon: 'paper-plane',
+    title: '治安防控',
+    icon: 'futbol-o',
     children: [
-      // { path: '/face-search', title: '结构化监控查询', icon: 'gg-circle' },
-      // { path: '/face-match', title: '通过人脸查询视频', icon: 'newspaper-o' },
-      { path: '/multiline', title: '地图轨迹融合', icon: 'yelp' },
-      { path: '/face-match', title: '人像轨迹搜索', icon: 'newspaper-o' }
-      // { path: '/face-search-by-face', title: '通过位置查询视频', icon: 'yelp' },
+      { title: '智慧小区',
+        icon: 'universal-access',
+        children: [
+          {
+            title: '监控布控中心',
+            icon: 'television',
+            children: [
+              { path: '/liveview', title: '实时监控显示', icon: 'play' },
+              { path: '/real-video', title: '地图布控显示', icon: 'support' },
+              { path: '/stream-list', title: '历史监控查询', icon: 'magic' }
+            ]
+          },
+          {
+            title: '情报研判中心',
+            icon: 'codiepie',
+            children: [
+              { path: '/testws', title: '人车情报研判中心', icon: 'camera' },
+              { path: '/structured_video', title: '视频结构化展示', icon: 'gg-circle' }
+            ]
+          },
+          {
+            title: '轨迹追踪',
+            icon: 'paper-plane',
+            children: [
+              { path: '/multiline', title: '地图轨迹融合', icon: 'yelp' },
+              { path: '/face-match', title: '人像轨迹搜索', icon: 'newspaper-o' }
+            ]
+          }
+        ]
+      },
+      { title: '视频融合中', icon: 'link' },
+      { title: '三位立体警务巡防系统', icon: 'tint' }
     ]
   },
   {
-    title: '人车情报研判',
-    icon: 'codiepie',
-    children: [
-      // { path: '/face-search', title: '结构化监控查询', icon: 'gg-circle' },
-      // { path: '/face-match', title: '通过人脸查询视频', icon: 'newspaper-o' },
-      // { path: '/realTime', title: '人车情报研判中心', icon: 'camera' },
-      // { path: '/match_search', title: '结构化监控查询', icon: 'gg-circle' },
-      { path: '/testws', title: '人车情报研判中心', icon: 'camera' },
-      { path: '/structured_video', title: '视频结构化展示', icon: 'gg-circle' }
-      // { path: '/face-search-by-face', title: '通过位置查询视频', icon: 'yelp' },
-    ]
-  },
-  {
-    title: '监控管理',
-    icon: 'superpowers',
-    children: [
-      { path: '/stream-catch', title: '监控布控显示', icon: 'university' },
-      { path: '/camera-detect', title: '监控检测管理', icon: 'yelp' },
-      // { path: '/liveview', title: '实时监控管理', icon: 'play' },  //先暂时隐藏掉
-      // { path: '/camera', title: '实时摄像头预览', icon: 'play' },
-      { path: '/camera-check', title: '检测运维', icon: 'cogs' }
-      // { path: '/face-websocket', title: 'websocket测试', icon: 'gg-circle' },
-      // { path: '/test-realTime', title: 'test-realTime', icon: 'camera' }
-    ]
-  },
-  {
-    title: '预警管理',
+    title: '预警事件中心',
     icon: 'bell-o',
     children: [
       { path: '/early-warning', title: '预警事件管理', icon: 'bell' },
       { path: '/warning-query', title: '预警事件查询', icon: 'share-alt-square' }
     ]
   },
+  demoFrame,
   {
     title: '系统管理',
     icon: 'gear',
     children: [
       { path: '/user', title: '用户管理', icon: 'user' },
       { path: '/face-list', title: '人脸库管理', icon: 'address-card' },
-      { path: '/camera', title: '摄像头管理', icon: 'video-camera' },
-      { path: '/watermark', title: '水印设置', icon: 'bookmark' }
+      // { path: '/camera', title: '摄像头管理', icon: 'video-camera' },
+      { path: '/camera-detect', title: '摄像头管理', icon: 'video-camera' },
+      { path: '/watermark', title: '水印设置', icon: 'bookmark' },
+      {
+        title: '日志管理',
+        icon: 'file-text',
+        children: [
+          { path: '/sys-log-login', title: '登录日志', icon: 'file-word-o' },
+          { path: '/sys-log-operation', title: '操作日志', icon: 'file-archive-o' },
+          { path: '/sys-log-error', title: '异常日志', icon: 'file-o' }
+        ]
+      },
+      {
+        title: '数据库管理',
+        icon: 'database',
+        children: [
+          { path: '/sensitive-database', title: '敏感数据库管理', icon: 'cube' },
+          { path: '/whitelist-database', title: '白名单数据库管理', icon: 'cubes' }
+        ]
+      }
     ]
-  },
-  {
-    title: '日志管理',
-    icon: 'file-text',
-    children: [
-      { path: '/sys-log-login', title: '登录日志', icon: 'file-word-o' },
-      { path: '/sys-log-operation', title: '操作日志', icon: 'file-archive-o' },
-      { path: '/sys-log-error', title: '异常日志', icon: 'file-o' }
-    ]
-  },
-  {
-    title: '数据库管理',
-    icon: 'database',
-    children: [
-      { path: '/sensitive-database', title: '敏感数据库管理', icon: 'cube' },
-      { path: '/whitelist-database', title: '白名单数据库管理', icon: 'cubes' }
-    ]
-  },
-  {
-    title: '大数据分析平台',
-    icon: 'linode',
-    children: [
-      { path: '/wea-analyse', title: '社区水电气分析', icon: 'tint' },
-      { path: '/camera-analyse', title: '影像数据分析', icon: 'file-video-o' }
-    ]
-  },
-  demoFrame
+  }
 ]
