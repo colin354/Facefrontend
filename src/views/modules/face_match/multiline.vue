26e36edf058b8b6bd218a98521e34c42cadb3402
lichao <xiaochaoli212@163.com>
2020-01-19 18:07:37 +0800

    multiline

--- src/views/modules/face_match/multiline.vue
+++ src/views/modules/face_match/multiline.vue
@@ -89,190 +89,187 @@ import mixinViewModule from '@/mixins/view-module'
 // NPM 方式
 import { lazyAMapApiLoaderInstance } from 'vue-amap';
 // import carUrl from '../../assets/images/carame.png'
-    export default {
-        name: "multiline",
-        // mixins: [ mixinViewModule ],
-        data () {
-            return {
-                showA:false,
-                markers:[],
-                //日期选项
-                pickerOptions: {
-                  shortcuts: [{
-                    text: '最近一周',
-                    onClick(picker) {
-                      const end = new Date();
-                      const start = new Date();
-                      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-                      picker.$emit('pick', [start, end]);
-                    }
-                  }, {
-                    text: '最近一个月',
-                    onClick(picker) {
-                      const end = new Date();
-                      const start = new Date();
-                      start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-                      picker.$emit('pick', [start, end]);
-                    }
-                  }, {
-                    text: '最近三个月',
-                    onClick(picker) {
-                      const end = new Date();
-                      const start = new Date();
-                      start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-                      picker.$emit('pick', [start, end]);
-                    }
-                  }]
-                },
-                value1: '',
-                //下拉框选项
-                selectedA:'数据类型',
-                options:[
-                    {value:'选项1',label:'车辆'},
-                    {value:'选项2',label:'人脸'}
-                ],
-                value:'',
-                paths:[],
-                temp:{},
-                dataList:[]
-            }
-        },
-        created() {
-            let self = this
-            this.initMap();
-            console.log("------我的弧度-------")
-            this.initPage();
-        },
-        methods:{  
-            initMap(){
-                lazyAMapApiLoaderInstance.load().then(() => {
-                    this.map = new AMap.Map('amap-show', {
-                        // center: [114.037939,22.627198],//自带
-                        // center: [120.09465,33.313217],  //盐城
-                        // center: [120.094163,33.313109],//汇文公馆
-                        center:[120.095913,33.302156],
-                        zoom: 18
-                    })
-                });
-            },
-            createTrackMap(){
-                this.initPage();
-                this.addMarker();
-            },
-            addMarker(){//画轨迹的同时,添加摄像头图标
-              let self = this
-              var startIcon = new AMap.Icon({ //摄像头图标
-                  // 图标尺寸
-                  size: new AMap.Size(25, 34),
-                  // 图标的取图地址
-                  image: '',//此处修改摄像头图标
-                  // 图标所用图片大小
-                  imageSize: new AMap.Size(20, 20),
-                  // 图标取图偏移量
-                  imageOffset: new AMap.Pixel(-1, -1)
-              });
-              if(this.markers){                
-                for(var i=0 ; i < this.markers.length ; i++){
-                  var viaMarker1 = new AMap.Marker({
-                      position:new AMap.LngLat(self.markers[i][0],self.markers[i][1]),
-                      icon: startIcon,
-                      offset:new AMap.Pixel(-10,-10)
-                    })
-                  this.map.add([viaMarker1])
-                }
-              }                            
-            },
-
-            
-            findTrackData(){
-                this.showA = true
-                this.getDurationGPSData(); //发送get请求
-            },
-            //点击“查询”,发送get请求           
-            getDurationGPSData(){
-                this.$axios.get("sys/check").then(res=> {
-                    console.log("------------res-----")
-                    console.log(res)
-                    console.log("------------res-----")
-                    this.temp = []  //存放轨迹的经纬度
-                    this.dataList = []
-                    this.markers = [] //存放摄像头经纬度
-                    this.temp = res.list
-                    this.markers = res.Markers
-                    this.dataList = res.list
-                    console.log(this.temp)                  
-                    console.log('可以正常打印不-------')
-                }).catch(error =>{
-                    console.log(error)
-                }) 
-            },
-            // getColor() {  //随机取不同的颜色
-            //     //北京地铁线的颜色
-            //     // var str0 = ["#a3308","#005195","#008998","#ae005f","#ba8b00","#c94d00","#009974","#C2C2C2","#CD00CD","#CD6600",
-            //     //             "#8fd400","#0095c3","#f5d312","#b6985e","#682145","#a192b4","#8F8F8F","#98F5FF","#8B1A1A","#545454",
-            //     //             "#4876FF","#8B0000","#8B6508","#8B8386","#B03060","#BCEE68","#C1CDCD","#CD8C95","#DEB887","#EE30A7"]
-            //     // // return str0[Math.floor(Math.random()*str0.length)]//从已有的颜色数组中随机选择,但容易不同轨迹是同一种颜色
-            //     // if(m < str0.length){
-            //     //   return str0[m]
-            //     // }else{
-            //     //   return str0[Math.floor(Math.random()*str0.length)]
-            //     // }
-            //     //随机函数获取的颜色
-            //     var str="#";
-            //     var arr=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
-            //     for(var i=0;i<6;i++){
-            //         var num=parseInt(Math.random()*16);
-            //         str+=arr[num];
-            //     }
-            //     return str;
-            // },
-            //画多条轨迹
-            initPage(){
-                this.map = new AMap.Map('amap-show', {
-                                center: [120.095913,33.302156],
-                                zoom: 18
-                            },
-                    AMapUI.loadUI(['misc/PathSimplifier'], (PathSimplifier) => {
-                        if (!PathSimplifier.supportCanvas) {
-                            alert('当前环境不支持 Canvas！');
-                            return;
-                        }
-                        for(var i=0 ; i<this.temp.length; i++){
-                            // console.log("------外层for----")
-                            // console.log(this.temp[i].location)
-                            // console.log(this.temp[i].color)
-                            var tempColor = this.temp[i].color  //同一个i值即同一个faceid,同一个人,用同一种颜色;不同的i值用不同的颜色
-                            var bezierCurve = new AMap.BezierCurve({
-                                path: this.temp[i].location,
-                                showDir: true,
-                                dirColor:'white',
-                                isOutline: true,
-                                outlineColor: "transparent",
-                                borderWeight: 3,
-                                strokeColor: tempColor, //线颜色
-                                strokeOpacity: 1,//线透明度
-                                strokeWeight: 5, //线宽
-                                // 线样式还支持 'dashed'
-                                strokeStyle: "solid", //线样式
-                                // strokeStyle是dashed时有效
-                                strokeDasharray: [10, 10],
-                                lineJoin: 'round',
-                                lineCap: 'round',
-                                zIndex: 50,
-                                dirArrowStyle:{
-                                    stepSpace: 8,  //stepSpace: {number} 箭头排布的间隔，单位像素
-                                    width: 3
-                                }                                   
-                            })
-                            bezierCurve.setMap(this.map)
-                            // 缩放地图到合适的视野级别
-                            this.map.setFitView([ bezierCurve ])
-                        }                                                       
-                    })
-                )
-            },
-        }
+export default {
+  name: "multiline",
+  // mixins: [ mixinViewModule ],
+  data () {
+    return {
+      showA:false,
+      markers:[],
+      //日期选项
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick (picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近一个月',
+          onClick (picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近三个月',
+          onClick (picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+            picker.$emit('pick', [start, end]);
+          }
+        }]
+      },
+      value1: '',
+      //下拉框选项
+      selectedA:'数据类型',
+      options:[
+        { value: '选项1', label: '车辆' },
+        { value: '选项2', label: '人脸' }
+      ],
+      value: '',
+      paths:[],
+      temp:{},
+      dataList:[]
     }
+  },
+  created () {
+    let self = this
+    this.initMap()
+    console.log("------我的弧度-------")
+    this.initPage()
+  },
+  methods:{  
+    initMap () {
+      lazyAMapApiLoaderInstance.load().then(() => {
+        this.map = new AMap.Map('amap-show', {
+          // center: [114.037939,22.627198],//自带
+          // center: [120.09465,33.313217],  //盐城
+          // center: [120.094163,33.313109],//汇文公馆
+          center:[120.095913, 33.302156],
+          zoom: 18
+        })
+      })
+    },
+    createTrackMap () {
+      this.initPage()
+      this.addMarker()
+    },
+    addMarker () {//画轨迹的同时,添加摄像头图标
+      let self = this
+      var startIcon = new AMap.Icon({ //摄像头图标
+        // 图标尺寸
+        size: new AMap.Size(25, 34),
+        // 图标的取图地址
+        image: '', //此处修改摄像头图标
+        // 图标所用图片大小
+        imageSize: new AMap.Size(20, 20),
+        // 图标取图偏移量
+        imageOffset: new AMap.Pixel(-1, -1)
+      })
+      if (this.markers) {
+        for (var i=0; i < this.markers.length; i++){
+          var viaMarker1 = new AMap.Marker({
+            position:new AMap.LngLat(self.markers[i][0], self.markers[i][1]),
+            icon: startIcon,
+            offset:new AMap.Pixel(-10, -10)
+          })
+          this.map.add([viaMarker1])
+        }
+      }
+    },
+    findTrackData () {
+      this.showA = true
+      this.getDurationGPSData(); //发送get请求
+    },
+    //点击“查询”,发送get请求           
+    getDurationGPSData () {
+      this.$axios.get("sys/check").then(res=> {
+        console.log("------------res-----")
+        console.log(res)
+        this.temp = []  //存放轨迹的经纬度
+        this.dataList = []
+        this.markers = [] //存放摄像头经纬度
+        this.temp = res.list
+        this.markers = res.Markers
+        this.dataList = res.list
+        console.log(this.temp)                  
+        console.log('可以正常打印不-------')
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    // getColor() {  //随机取不同的颜色
+    //     //北京地铁线的颜色
+    //     // var str0 = ["#a3308","#005195","#008998","#ae005f","#ba8b00","#c94d00","#009974","#C2C2C2","#CD00CD","#CD6600",
+    //     //             "#8fd400","#0095c3","#f5d312","#b6985e","#682145","#a192b4","#8F8F8F","#98F5FF","#8B1A1A","#545454",
+    //     //             "#4876FF","#8B0000","#8B6508","#8B8386","#B03060","#BCEE68","#C1CDCD","#CD8C95","#DEB887","#EE30A7"]
+    //     // // return str0[Math.floor(Math.random()*str0.length)]//从已有的颜色数组中随机选择,但容易不同轨迹是同一种颜色
+    //     // if(m < str0.length){
+    //     //   return str0[m]
+    //     // }else{
+    //     //   return str0[Math.floor(Math.random()*str0.length)]
+    //     // }
+    //     //随机函数获取的颜色
+    //     var str="#";
+    //     var arr=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+    //     for(var i=0;i<6;i++){
+    //         var num=parseInt(Math.random()*16);
+    //         str+=arr[num];
+    //     }
+    //     return str;
+    // },
+    //画多条轨迹
+    initPage () {
+      this.map = new AMap.Map('amap-show', {
+        center: [120.095913, 33.302156],
+        zoom: 18
+      },
+      AMapUI.loadUI(['misc/PathSimplifier'], (PathSimplifier) => {
+        if (!PathSimplifier.supportCanvas) {
+          alert('当前环境不支持 Canvas！');
+          return;
+        }
+        for(var i=0 ; i<this.temp.length; i++){
+          // console.log("------外层for----")
+          // console.log(this.temp[i].location)
+          // console.log(this.temp[i].color)
+          var tempColor = this.temp[i].color  //同一个i值即同一个faceid,同一个人,用同一种颜色;不同的i值用不同的颜色
+          var bezierCurve = new AMap.BezierCurve({
+            path: this.temp[i].location,
+            showDir: true,
+            dirColor:'white',
+            isOutline: true,
+            outlineColor: "transparent",
+            borderWeight: 3,
+            strokeColor: tempColor, //线颜色
+            strokeOpacity: 1, //线透明度
+            strokeWeight: 5, //线宽
+            // 线样式还支持 'dashed'
+            strokeStyle: "solid", //线样式
+            // strokeStyle是dashed时有效
+            strokeDasharray: [10, 10],
+            lineJoin: 'round',
+            lineCap: 'round',
+            zIndex: 50,
+            dirArrowStyle:{
+              stepSpace: 8,  //stepSpace: {number} 箭头排布的间隔，单位像素
+              width: 3
+            }
+          })
+          bezierCurve.setMap(this.map)
+          // 缩放地图到合适的视野级别
+          this.map.setFitView([ bezierCurve ])
+        }                                                       
+      })
+      )
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
